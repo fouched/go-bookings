5ede0dc56b0453d60333ea5f3045962958c6f26e
Fouche du Preez <fouche@limehouse.co.za>
2024-09-29 13:02:52 +0200

    updated admin pages

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -46,6 +46,8 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/reservations-new", handlers.Repo.AdminReservationsNew)
 		mux.Get("/reservations-all", handlers.Repo.AdminReservationsAll)
 		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
+		mux.Get("/process-reservation/{src}/{id}", handlers.Repo.AdminProcessReservation)
+		mux.Get("/delete-reservation/{src}/{id}", handlers.Repo.AdminDeleteReservation)
 
 		mux.Get("/reservations/{src}/{id}", handlers.Repo.AdminReservationShow)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostReservation)
